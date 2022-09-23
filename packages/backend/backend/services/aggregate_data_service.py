b6a0cb84e6d524b6b164043ea8648a13020f593e
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-09-24 08:41:08 +0900

    feat: リポジトリ層のモックを作成

--- packages/backend/backend/services/aggregate_data_service.py
+++ packages/backend/backend/services/aggregate_data_service.py
@@ -1,7 +1,7 @@
 class AggregateDataService:
     """集計データの取得に関する処理をここで行う
-    - 滞在データの取得
-    - 宿泊データの取得
+    - 滞在データの取得と集計（グルーピング）
+    - 宿泊データの取得と集計（グルーピング）
     """
 
     def get_stays_data(self, user_id: int):
