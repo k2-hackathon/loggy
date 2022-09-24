d764dc0be29e8000662190697c23aa7c3850b20c
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-09-24 13:37:29 +0900

    feat: commands dirの追加

---
+++ packages/backend/backend/commands/aggregator.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Aggregator:
+    def __init__(self):
+        # TODO: self.access_key = dynamoDB接続キー etc.
+        # TODO: self.access_key = RDB接続キー etc.
+        ...
+
+    def get_geography(self):
+        """DynamoDBからデータを取得する"""
+        # data = sqlAlchemyなどのORMapperを使う
+        ...
+
+    def aggregate_lodgings(self, data: List):
+        """lodgingsのデータを集計するメソッド"""
+        ...
+
+    def create_lodgings(self, agg_data: List):
+        """集計されたlodgingsのデータをRDBに書き込むメソッド"""
+        ...
+
+    def aggregate_stays(self, data: List):
+        """staysのデータを集計するメソッド"""
+        ...
+
+    def create_stays(self, agg_data: List):
+        """集計されたのデータをRDBに書き込むメソッド"""
+        ...
+
+
+def aggregator():
+    agg = Aggregator()
+    data = agg.get_geography()
+
+    agg_lodge_data = agg.aggregate_lodgings(data)
+    agg.create_lodgings(agg_lodge_data)
+
+    agg_stays_data = agg.aggregate_stays(data)
+    agg.create_stays(agg_stays_data)
+    print("command success")
--- packages/backend/backend/main.py
+++ packages/backend/backend/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from api.aggregates import router as aggregates
 from api.users import router as users
 from api.geography import router as geography
+from typer import Typer
+from commands.aggregator import aggregator
+
 
 app = FastAPI()
 
 app.include_router(aggregates.router)
 app.include_router(users.router)
 app.include_router(geography.router)
+
+
+if __name__ == "__main__":
+    app = Typer()
+    app.command()(aggregator)
+    app()
