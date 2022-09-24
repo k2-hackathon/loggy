b22908610c9d6dfb6a57125ce105f97c46a2d13a
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-09-24 12:18:19 +0900

    feat: aggregates dirの作成

---
+++ packages/backend/backend/api/aggregates/services/aggregates.py
@@ -0,0 +1,13 @@
+class AggregateService:
+    """集計データの取得に関する処理をここで行う
+    - 滞在データの取得と集計（グルーピング）
+    - 宿泊データの取得と集計（グルーピング）
+    """
+
+    def get_stays_data(self, user_id: int):
+        # TODO: リポジトリ層のpostgresqlからstaysの集計データを取得する
+        pass
+
+    def get_lodgings_data(self, user_id: int):
+        # TODO: リポジトリ層のpostgresqlからlodgingsの集計データを取得する
+        pass
