8dd057b9419fba4a3a5055ba36cb75b5b62fe5f4
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-09-26 08:26:44 +0900

    feat: healthcheckエンドポイントの作成

--- packages/backend/backend/main.py
+++ packages/backend/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.aggregates import router as aggregates
 from api.users import router as users
 from api.geography import router as geography
+from api import health_check
 from typer import Typer
 from commands.aggregator import aggregator
 
@@ -11,7 +12,7 @@ app = FastAPI()
 app.include_router(aggregates.router)
 app.include_router(users.router)
 app.include_router(geography.router)
-
+app.include_router(health_check.router)
 
 if __name__ == "__main__":
     app = Typer()
