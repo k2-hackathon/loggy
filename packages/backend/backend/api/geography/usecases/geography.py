fe5af3e6feca67fa5349c81bbc48970f5e88efbd
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-09-26 22:55:04 +0900

    fix: user_idのハッシュ化を止める

--- packages/backend/backend/api/geography/usecases/geography.py
+++ packages/backend/backend/api/geography/usecases/geography.py
@@ -1,13 +1,12 @@
 from ..services.geography import GeographyService
 from datetime import datetime
-import hashlib
 from ..models.geography import GeographyModel
 
 
 def create_geography(user_id: str, longitude: str, latitude: str, date: datetime):
     timestamp = date.timestamp()
     request_data = GeographyModel(
-        user_id=hashlib.md5(user_id.encode()).hexdigest(),
+        user_id=user_id,
         longitude=longitude,
         latitude=latitude,
         timestamp=timestamp,
