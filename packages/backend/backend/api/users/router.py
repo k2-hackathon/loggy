7d2449b447e12af9cba25cbd9e83d3889006ebd2
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-10-04 20:00:30 +0900

    fix: 登録・取得するUserは一人なので単数形に変更

--- packages/backend/backend/api/users/router.py
+++ packages/backend/backend/api/users/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from api.users.usecases.users import get_users, create_users
+from api.users.usecases.users import get_user, create_user
 
 router = APIRouter()
 
-router.get("/users/{user_id}")(get_users)
-router.post("/users/{user_id}")(create_users)
+router.get("/users/{user_id}")(get_user)
+router.post("/users/{user_id}")(create_user)
--- packages/backend/backend/api/users/usecases/users.py
+++ packages/backend/backend/api/users/usecases/users.py
@@ -2,12 +2,12 @@ from api.users.services.users import UserDataService
 from api.users.models.users import GetUserRequestModel, CreateUserRequestModel
 
 
-def get_users(user_id: str) -> str:
+def get_user(user_id: str) -> str:
     service = UserDataService()
     response = service.get_user_profile(GetUserRequestModel(user_id=user_id))
     return response
 
 
-def create_users(user_id: str, name: str, mail: str) -> None:
+def create_user(user_id: str, name: str, mail: str) -> None:
     service = UserDataService()
     service.create_user(CreateUserRequestModel(user_id=user_id, name=name, mail=mail))
